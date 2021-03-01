c83eb1b33a93f03df5e10714ecc486042e124e3f
Toni Illokoski <toni.illokoski@gmail.com>
2021-03-01 22:07:38 +0200

    Fixed sunrise/sunset time

--- index.js
+++ index.js
@@ -11,16 +11,24 @@ $.getJSON(
         //Temperature feels like
         let feels = data.main.feels_like;
         //Sunrise
-        let sunRise = data.sys.sunrise;
+        let sunRise = new Date(data.sys.sunrise);
         //Sunset
         let sunSet = data.sys.sunset;
+  
+        let sunRise2 = new Date(sunRise * 1000);
+        let sunRise3 = sunRise2.toLocaleTimeString();
+
+        let sunSet2 = new Date(sunSet * 1000);
+        let sunSet3 = sunSet2.toLocaleTimeString();
+
+
 
         $("#icon").attr('src', icon);
-        document.getElementById("temp").innerHTML = "There is " + temp + "°C";
+        document.getElementById("temp").innerHTML = temp + "°C <br>";
         document.getElementById("feels").innerHTML = "Feels like " + feels + "°C";
 
-        document.getElementById("sunrise").innerHTML = "Sun rises " + sunRise;
-        document.getElementById("sunset").innerHTML = "Sun sets " + sunSet;
+        document.getElementById("sunrise").innerHTML = "Sun rises " + sunRise3;
+        document.getElementById("sunset").innerHTML = "Sun sets " + sunSet3;
         
         
 
